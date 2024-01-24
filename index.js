947b3f518972020cb6457d151bae65372bc596a4
Kittipong1939 <kittipong.2020@hotmail.com>
2024-01-24 22:33:10 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
-const express = require("rxpress");
+const express = require("express");
+const authRoute = require("./routes/auth-route");
+const postRoute = require("./routes/post-route");
 
+const errorHandler = require("./middlewares/error");
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(8000,()=>{
-    console.log("sever run on port 8000")
+app.use("/auth", authRoute);
+app.use("/post", postRoute);
+
+app.use(errorHandler);
+
+app.listen(8000, () => {
+  console.log("Server run on port 8000");
 });
